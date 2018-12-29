98ba9f1fb759e7af645b585289ff0fdb5bc4a8b1
3years5hours <46236462+3years5hours@users.noreply.github.com>
2018-12-29 16:25:44 +0900

    Update nokori.js

--- nokori.js
+++ nokori.js
@@ -36,13 +36,10 @@
                         break;
                
         }
-        
-        document.write( string );
-document.write(‘\
-\
-example\
-foo bar\
-\
-‘);
+        a="※受付終了の場合は、予約注文となり翌月曜日に発送いたします。"
+        b="12月30日～1月3日はお休みとなります。12月29日以降のご注文は、1月4日発送となります。"
+        document.write(string+<BR>b>);
+        //document.write( string );
+
 -->
 /*]]>*/
